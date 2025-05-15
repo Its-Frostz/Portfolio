fdc485da7cd98c66c02c8c581b120d6152627803
Atif Ahmed <thecoldqrage@gmail.com>
2025-05-16 05:17:01 +0530

    small fix with mobike view

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -38,6 +38,7 @@ export default function About() {
                   </li>
                 );
               })}
+              <li></li>
             </ul>
           </div>
         </TextBlock>
