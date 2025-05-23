5fe55bdaec4c49289c12211297899bd4779196fb
Atif Ahmed <thecoldqrage@gmail.com>
2025-05-23 21:39:43 +0530

    Updated the Gsap animation to modern convention

--- src/components/Toons/Potion/Potion.jsx
+++ src/components/Toons/Potion/Potion.jsx
@@ -10,55 +10,55 @@ import { LOOP_EASE_IN_OUT } from "@/Constants.jsx";
 import "@/css/Components/toons/Potion.scss";
 
 export default function Potion({ isPlaying }) {
-    const svgRef = useToonAnimation(isPlaying, (svg, loop) => {
-      const potion = getNodes("#potion");
-      loop
-        .to(potion, 3, {
-          transformOrigin: "50% 90%",
-          rotation: 15,
-          ...LOOP_EASE_IN_OUT,
+  const svgRef = useToonAnimation(isPlaying, (svg, loop) => {
+    const potion = getNodes("#potion");
+    loop
+      .addLabel("start")
+      .to(potion, {
+        duration: 3,
+        transformOrigin: "50% 90%",
+        rotation: 15,
+        ...LOOP_EASE_IN_OUT,
+      })
+      .fromTo(
+        potion.potionDrop,
+        {
+          yPercent: 100,
+          rotation: 0,
+          scale: 1,
+        },
+        {
+          duration: 0.5,
+          transformOrigin: "20% 120%",
+          rotation: 360,
+          yPercent: -100,
+          scale: 0,
+          repeat: -1,
         },
         "start"
       )
-        .fromTo(
-          potion.potionDrop,
-          0.5,
-          {
-            yPercent: 100,
-            rotation: 0,
-            scale: 1,
-          },
-          {
-            transformOrigin: "20% 120%",
-            rotation: 360,
-            yPercent: -100,
-            scale: 0,
-            repeat: -1,
-          },
-          "start"
-        )
-        .fromTo(
-          potion.potionDrop2,
-          0.7,
-          {
-            yPercent: 100,
-            rotation: 0,
-            scale: 1,
-          },
-          {
-            transformOrigin: "0% 120%",
-            rotation: -360,
-            yPercent: -100,
-            scale: 0,
-            repeat: -1,
-          },
-          "start"
-        );
-    });
+      .fromTo(
+        potion.potionDrop2,
+        {
+          yPercent: 100,
+          rotation: 0,
+          scale: 1,
+        },
+        {
+          duration: 0.7,
+          transformOrigin: "0% 120%",
+          rotation: -360,
+          yPercent: -100,
+          scale: 0,
+          repeat: -1,
+        },
+        "start"
+      );
+  });
 
-    return (
-        <div>
-            <PotionSVG ref={svgRef}/>
-        </div>
-    );
+  return (
+    <div>
+      <PotionSVG ref={svgRef} />
+    </div>
+  );
 }
