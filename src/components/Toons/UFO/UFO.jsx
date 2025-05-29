6db11dad0815ab2a9f7ee4663667764a1bc3704c
Atif Ahmed <thecoldqrage@gmail.com>
2025-05-29 21:14:03 +0530

    Added Trish... :)

---
+++ src/components/Toons/UFO/UFO.jsx
@@ -0,0 +1,85 @@
+// Svg component
+import UFOSVG from "./UFOSVG.jsx";
+
+// Animation stuff for toon
+import { useToonAnimation } from "../animationHook.jsx";
+import { LOOP_EASE_IN_OUT, LOOP } from "@/Constants.jsx";
+import { getNodes, Random } from "@/utils.jsx";
+
+export default function UFO({ isPlaying }) {
+  const svgRef = useToonAnimation(isPlaying, (svg, tl) => {
+    const trish = getNodes("#trish");
+    tl.clear()
+      .addLabel("start", 0)
+      .to(
+        trish.ship,
+        {
+          duration: 0.25,
+          rotationZ: 5,
+          transformOrigin: "50% 75%",
+          ...LOOP_EASE_IN_OUT,
+        },
+        "start"
+      )
+      .fromTo(
+        trish.eye,
+        {
+          duration: 0.05,
+          scaleY: 0.1,
+          transformOrigin: "50% 50%",
+        },
+        {
+          scaleY: 1,
+          repeat: -1,
+          repeatDelay: 1.618,
+        },
+        "start"
+      )
+      .to(
+        trish.trish,
+        {
+          duration: 12,
+          motionPath: {
+            path: [
+              {
+                xPercent: Random(-100, 50),
+                yPercent: Random(-40, 40),
+                rotationZ: Random(15, 30),
+              },
+              {
+                xPercent: Random(-100, 50),
+                yPercent: Random(-40, 40),
+                rotationZ: Random(-30, -15),
+              },
+              {
+                xPercent: Random(-100, 50),
+                yPercent: Random(-40, 40),
+                rotationZ: Random(15, 30),
+              },
+              {
+                xPercent: Random(-100, 50),
+                yPercent: Random(-40, 40),
+                rotationZ: Random(-30, -15),
+              },
+              {
+                xPercent: Random(-100, 50),
+                yPercent: Random(-40, 40),
+                rotationZ: Random(15, 30),
+              },
+              {
+                xPercent: Random(-100, 50),
+                yPercent: Random(-40, 40),
+                rotationZ: Random(-30, -15),
+              },
+            ],
+            curviness: 1.25,
+            // autoRotate: true,
+          },
+          transformOrigin: "50% 5%",
+          ...LOOP,
+        },
+        "start"
+      );
+  });
+  return <UFOSVG ref={svgRef} />;
+}
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -5,17 +5,19 @@ import Shrimp from "@/components/Toons/Shrimp/Shrimp.jsx";
 import Astronaut from "@/components/Toons/Astronaut/Astronaut.jsx";
 import Shapes from "@/components/Toons/Shapes/Shapes.jsx";
 import CoffeeMug from "@/components/Toons/CoffeeMug/CoffeeMug.jsx";
+import UFO from "@/components/Toons/UFO/UFO.jsx";
 
 export default function Test() {
   return (
     <>
       {/* <Potion isPlaying={true} />
       <Hexo isPlaying={true} />
-      <Monsier isPlaying={true} />  */}
+      <Monsier isPlaying={true} /> 
       <Shrimp isPlaying={true} />
-      {/* <Astronaut isPlaying={true} />
+      <Astronaut isPlaying={true} />
       <Shapes isPlaying={true} />
       <CoffeeMug isPlaying={true} /> */}
+      <UFO isPlaying={true} />
     </>
   );
 }
