0a024f4de7fc8f675283d1ae357b6a5a52cb874a
Atif Ahmed <thecoldqrage@gmail.com>
2025-06-01 20:56:46 +0530

    Improved on the animation Hook so it has no more lingering svgs

--- src/components/Toons/animationHook.jsx
+++ src/components/Toons/animationHook.jsx
@@ -45,6 +45,12 @@ export function useToonAnimation(isPlaying, initFn, dependency = []) {
         isPlaying ? svg.unpauseAnimations() : svg.pauseAnimations();
       }
 
+      // Control autoalpha based on isPlaying state
+      gsap.to(svg, {
+        autoAlpha: isPlaying ? 1 : 0,
+        duration: 0.3
+      });
+
       // GSAP timeline
       isPlaying ? tl.play() : tl.pause();
     },
